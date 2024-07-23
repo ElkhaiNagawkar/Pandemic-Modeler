8f12c1cc162f20c0703710b2ebd429c4d6df119c
nagaw <nagaw@LAPTOP-N4TSRDJK>
2024-07-22 23:37:29 -0400

    testing push

---
+++ Pandemic Modeler/src/tester.java
@@ -0,0 +1,18 @@
+/**
+ * Program Name: tester.java
+ * Purpose: OK DUMMY, DON'T FORGET THIS!
+ * Coder: Elkhai Nagawkar (1115993) for Sec 04
+ * Date: Jul 22, 2024
+ */
+
+public class tester
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+	//end main
+}
+//end class
